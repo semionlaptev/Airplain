f596ee5efd9bb68d106d12e2246212e8ce7d9255
Semion Laptev <semionlaptev@gmail.com>
2011-09-17 22:09:35 +0400

    big big big commit

---
+++ Airplane/Airplane/IDrawable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Airplane
+{
+    public interface IDrawable
+    {
+        Texture2D Image { get; }
+        float Width { get; }
+        float Height { get; }
+        float ImageScale { get; }
+        Rectangle SourceRect { get; }
+        float ImageRotation { get; }
+        Vector2 ImageOrigin { get; }
+        Vector2 ImageSize { get; }
+        //void donotuse();
+    }
+}
---
+++ Airplane/Airplane/IGameDictionary.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Airplane
+{
+    interface IGameDictionary
+    {
+        void AddKeyVal(GameObject keyobj, GameObject valobj);
+        void RemoveByKey(GameObject keyobj);
+    }
+}
