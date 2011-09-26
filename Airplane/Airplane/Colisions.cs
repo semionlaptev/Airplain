7829aed9a548ddaa15aa166cb722e06b4b422e7c
Semion Laptev <semionlaptev@gmail.com>
2011-09-26 09:04:10 +0400

    Roma dima

---
+++ Airplane/Airplane/Colisions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Airplane
+{
+    static class Colisions
+    {
+        static public bool CheckCollision(DenseObject objOne, DenseObject objTwo)
+        {
+            return objOne.CollisionRectPositionedScaled.Intersects(objTwo.CollisionRectPositionedScaled);
+        }
+
+        static public List<DenseObject> CheckCollisions(DenseObject objOne, List<DenseObject> objects)
+        {
+            if (objects != null)
+            {
+                List<DenseObject> tempDenceObjects = new List<DenseObject>();
+                foreach (DenseObject obj in objects)
+                {
+                    if (CheckCollision(objOne, obj))
+                    {
+                        tempDenceObjects.Add(obj);
+                    }
+                }
+
+                if (tempDenceObjects.Count > 0)
+                {
+                    return tempDenceObjects;
+                }
+            }
+                return null;
+        }
+    }
+}
---
+++ Airplane/Airplane/Physx.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+using Microsoft.Xna.Framework;
+
+namespace Airplane
+{
+    class Physx:  IGameDictionary
+    {
+        //Храним ссылку на объект и результирующий вектор постоянно действующих сил
+        Dictionary<PositionedObject, Vector2> objectslist_ = new Dictionary<PositionedObject, Vector2>();
+
+        //Тут можно было бы хранить различные физические константы
+        //Можно запилить динамическую генерацию полей
+
+        public void addImpulse(PositionedObject obj, Vector2 impulse)
+        {
+            //Единичный импульс, добавляется к скорости объекта
+            obj.Speed += impulse;
+        }
+
+        public void addForce(PositionedObject obj, Vector2 force)
+        {
+            //Добавляем/изменяем постоянную силу, действующую на объект
+            if (objectslist_.ContainsKey(obj) == false)
+            {
+                objectslist_[obj] = new Vector2(force.X, force.Y);
+            }
+            else
+            {
+                objectslist_[obj] += force;
+            }
+        }
+
+        public void setForce(PositionedObject obj, Vector2 vector)
+        {
+            //Возможность напрямую установить значение постоянной силы
+            //Может пригодиться т.к. добавление и удаление сил производится математически
+            //И в случае возникновения остатка/математической ошибки вектор можно обнулить
+            //Бонус - телепортация
+            //Кстати, общение через каментарии в коде заставляет его читать >_<
+            objectslist_[obj] = vector;
+        }
+
+        public void doPhysix()
+        {
+            //Считаем физику
+            foreach (KeyValuePair<PositionedObject, Vector2> obj in objectslist_)
+            {
+                obj.Key.Speed += obj.Value;
+            }
+        }
+
+        public double Count()
+        {
+            return objectslist_.Count();
+        }
+
+        public void removeObject(PositionedObject obj)
+        {
+            //Удаляем объект и все силы, связанные с ним
+            if (objectslist_.ContainsKey(obj))
+            {
+                objectslist_.Remove(obj);
+            }
+        }
+
+        public void RemoveByKey(GameObject key)
+        {
+
+        }
+
+        public void AddKeyVal(GameObject obj, object val)
+        {
+
+        }
+
+
+    }
+}
